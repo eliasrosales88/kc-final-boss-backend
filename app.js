3feb24efd21caeb494dcd0e0cb350325bbf79058
Elias Rosales <eliasrosales88@gmgmail.com>
2020-01-29 22:03:17 +0100

    Initializing node project

---
+++ app.js
@@ -0,0 +1,104 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+const swaggerJsDocs = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+
+/***************
+ SWAGGER CONFIG
+ ***************/
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Wallaclone API',
+      description: 'Wallaclone API Information',
+      contact: {
+        name: 'github: eliasrosales88'
+      },
+      servers: ["http://localhost:3000"]
+    }
+  },
+  // apis: ['routes/*.js']
+  apis: ['app.js']
+};
+
+const swaggerDocs = swaggerJsDocs(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /customers:
+ *  get:
+ *    description: Get all users
+ *    responses:
+ *      '200':
+ *        description: Success
+ */
+app.get("/customers", (req, res) => {
+  console.log("request");
+  res.status(200).send("Customers");
+
+})
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+
+
+
+
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
